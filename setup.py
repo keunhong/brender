62b2126c1d2060edf7073b3fc8435ca8cb5c5035
Keunhong Park <kpar@cs.washington.edu>
2018-10-09 19:24:32 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'brender',
+  packages = ['brender'],
+  version = '0.1',
+  description = 'A simple wrapper for the Blender API (bpy)',
+  author = 'Keunhong Park',
+  author_email = 'kpar@cs.washington.edu',
+  url = 'https://github.com/keunhong/brender',
+  download_url = 'https://github.com/keunhong/brender/archive/0.1.tar.gz',
+  keywords = ['python', 'brender', 'blender'],
+  classifiers = [],
+)
+
